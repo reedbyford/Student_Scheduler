207256b6fb957d548b798b1282a64df320aca729
ReedmByford <reedbyford@gmail.com>
2023-01-29 22:54:33 -0600

    Initial commit

---
+++ app/src/main/java/android/reedbyford/studentscheduler/UI/TermList.java
@@ -0,0 +1,15 @@
+package android.reedbyford.studentscheduler.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.reedbyford.studentscheduler.R;
+
+public class TermList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_list);
+    }
+}
