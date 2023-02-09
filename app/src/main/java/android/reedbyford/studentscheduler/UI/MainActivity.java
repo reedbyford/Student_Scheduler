672669741e64ebe3059022401948fa8aed2ab77d
ReedmByford <reedbyford@gmail.com>
2023-02-08 23:10:34 -0600

    Create sample data menu

--- app/src/main/java/android/reedbyford/studentscheduler/UI/MainActivity.java
+++ app/src/main/java/android/reedbyford/studentscheduler/UI/MainActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.reedbyford.studentscheduler.R;
+import android.reedbyford.studentscheduler.database.Repository;
+import android.reedbyford.studentscheduler.entities.Course;
+import android.reedbyford.studentscheduler.entities.Term;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button=findViewById(R.id.button);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,5 +28,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addSampleData:
+                Term term=new Term(0, "Term 1", "09-01-2022", "02-28-2023");
+                Repository repository=new Repository(getApplication());
+                repository.insert(term);
+                Course course=new Course(0, "Introduction to IT - C182", "09-01-2022", "09-04-2023", "Completed", 0);
+                repository.insert(course);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
